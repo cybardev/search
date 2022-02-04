fc8d811bc82000faf2b4720d1c998cdc1976affd
Sheikh <cybardev@pm.me>
2022-02-04 16:04:34 -0400

    update

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // @ts-nocheck
 document.querySelector(".search-box")?.addEventListener("submit", (e) => {
     e.preventDefault();
-    window.location.replace(
+    window.location.href =
         "https://www.ecosia.org/search?q=" +
-            encodeURIComponent(e.currentTarget.searchInput.value)
-    );
+        encodeURIComponent(e.currentTarget.searchInput.value);
 });
