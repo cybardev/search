bec106bb5d8996cfe9c72ee930e7313845d0b5f6
Sheikh <cybardev@pm.me>
2022-02-04 23:16:05 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -5,23 +5,3 @@ document.querySelector(".search-box")?.addEventListener("submit", (e) => {
         "https://www.ecosia.org/search?q=" +
         encodeURIComponent(e.currentTarget.searchInput.value);
 });
-
-// Check that service workers are supported
-if ("serviceWorker" in navigator) {
-    // Use the window load event to keep the page load performant
-    window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/service-worker.js").then(
-            function (registration) {
-                console.log(
-                    "service worker registration succeeded:",
-                    registration
-                );
-            },
-            function (error) {
-                console.log("service worker registration failed:", error);
-            }
-        );
-    });
-} else {
-    console.log("service workers are not supported.");
-}
