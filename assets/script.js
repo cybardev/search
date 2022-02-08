3d9c1024d0139364acdc9c0f365dc09f8664ea68
Sheikh <cybardev@pm.me>
2022-02-08 15:31:44 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,22 @@
 // @ts-nocheck
+function search(str) {
+    let results;
+    fetch(`/.netlify/functions/search?query=${str}`)
+        .then((data) => data.json())
+        .then(({ res }) => (results = res))
+        .catch((err) => {
+            console.error(err);
+        });
+    return results;
+}
+
+const staticData = {
+    results: [],
+};
+
 document.querySelector(".search-box")?.addEventListener("submit", (e) => {
     e.preventDefault();
-    window.location.href =
-        "https://www.ecosia.org/search?q=" +
-        encodeURIComponent(e.currentTarget.searchInput.value);
+    staticData.results = search(
+        encodeURIComponent(e.currentTarget.searchInput.value)
+    );
 });
---
+++ netlify/functions/search.js
@@ -0,0 +1,37 @@
+// @ts-nocheck
+const fetch = require("node-fetch");
+
+exports.handler = async (event, context) => {
+    try {
+        const API_KEY = process.env.SEARCH_API,
+            API_HOST = "contextualwebsearch-websearch-v1.p.rapidapi.com",
+            API_ENDPOINT =
+                "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI?q=",
+            SEARCH_PARAMS =
+                "&pageNumber=1&pageSize=50&autoCorrect=false&safeSearch=false",
+            QUERY_STRING = event.queryStringParameters.query;
+
+        let search_results;
+
+        fetch(API_ENDPOINT + QUERY_STRING + SEARCH_PARAMS, {
+            method: "GET",
+            headers: {
+                "x-rapidapi-host": API_HOST,
+                "x-rapidapi-key": API_KEY,
+            },
+        }).then((response) => {
+            search_results = response.json().value;
+        });
+
+        console.log(search_results);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                res: search_results,
+            }),
+        };
+    } catch (err) {
+        return { statusCode: 500, body: err.toString() };
+    }
+};
