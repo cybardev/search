18172fdac7c6227b2942aefda35043823b321968
Sheikh <cybardev@pm.me>
2022-02-26 17:34:22 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,25 @@
-// @ts-nocheck
+/**
+ * Script to call API endpoint and store search results
+ */
 const staticData = {
     results: [],
-};
-
-async function callAPI(event) {
-    try {
-        const response = await fetch(
-            `/.netlify/functions/search?query=${encodeURIComponent(
-                event.currentTarget.query.value
-            )}`
-        );
-        if (!response.ok) {
-            // NOT res.status >= 200 && res.status < 300
-            console.log(response.toString());
+    callAPI: async (event) => {
+        try {
+            const response = await fetch(
+                `/.netlify/functions/search?query=${encodeURIComponent(
+                    event.currentTarget.query.value
+                )}`
+            );
+            if (!response.ok) {
+                // NOT res.status >= 200 && res.status < 300
+                console.log(response.toString());
+            }
+            const data = await response.json();
+            staticData.results = data.value;
+            console.log(staticData.results);
+        } catch (error) {
+            // output to console log
+            console.log(error.toString());
         }
-        const data = await response.json();
-        staticData.results = data.value;
-        console.log(staticData.results);
-    } catch (error) {
-        // output to console log
-        console.log(error.toString());
-    }
-}
+    },
+};
