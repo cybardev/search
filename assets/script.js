8c7288ad95c41bc5221c1ef3ebd01dfb4d4b7d2b
Sheikh <cybardev@pm.me>
2022-02-26 13:14:11 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,27 @@
 // @ts-nocheck
-
 const staticData = {
     results: [],
 };
 
-document.querySelector(".search-box")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    try {
-        const response = await fetch(
-            `/.netlify/functions/search?query=${encodeURIComponent(
-                e.currentTarget.searchInput.value
-            )}`
-        );
-        if (!response.ok) {
-            // NOT res.status >= 200 && res.status < 300
-            console.log(response.toString());
+document
+    .querySelector(".search-box")
+    ?.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch(
+                `/.netlify/functions/search?query=${encodeURIComponent(
+                    event.currentTarget.searchInput.value
+                )}`
+            );
+            if (!response.ok) {
+                // NOT res.status >= 200 && res.status < 300
+                console.log(response.toString());
+            }
+            const data = await response.json();
+            console.log(data);
+            staticData.results = data.value;
+        } catch (error) {
+            // output to console log
+            console.log(error.toString());
         }
-        const data = await response.json();
-        console.log(data);
-        staticData.results = data.value;
-    } catch (error) {
-        // output to console log
-        console.log(error.toString());
-    }
-});
+    });
