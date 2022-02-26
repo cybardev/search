dd8bebd09545ab870e057f9f964f34696ea637db
Sheikh <cybardev@pm.me>
2022-02-26 16:39:09 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,27 @@
 // @ts-nocheck
 const staticData = {
     results: [],
-    async callAPI(event) {
-        try {
-            const response = await fetch(
-                `/.netlify/functions/search?query=${encodeURIComponent(
-                    event.currentTarget.searchInput.value
-                )}`
-            );
-            if (!response.ok) {
-                // NOT res.status >= 200 && res.status < 300
-                console.log(response.toString());
-            }
-            const data = await response.json();
-            staticData.results = data.value;
-            console.log(staticData.results);
-        } catch (error) {
-            // output to console log
-            console.log(error.toString());
-        }
-    },
 };
 
 // document.querySelector(".search-box").addEventListener("submit", callAPI);
 
-// async function callAPI(event) {
-//     // event.preventDefault();
-//     try {
-//         const response = await fetch(
-//             `/.netlify/functions/search?query=${encodeURIComponent(
-//                 event.currentTarget.searchInput.value
-//             )}`
-//         );
-//         if (!response.ok) {
-//             // NOT res.status >= 200 && res.status < 300
-//             console.log(response.toString());
-//         }
-//         const data = await response.json();
-//         staticData.results = data.value;
-//         console.log(staticData.results);
-//     } catch (error) {
-//         // output to console log
-//         console.log(error.toString());
-//     }
-// }
+async function callAPI(event) {
+    // event.preventDefault();
+    try {
+        const response = await fetch(
+            `/.netlify/functions/search?query=${encodeURIComponent(
+                event.currentTarget.searchInput.value
+            )}`
+        );
+        if (!response.ok) {
+            // NOT res.status >= 200 && res.status < 300
+            console.log(response.toString());
+        }
+        const data = await response.json();
+        staticData.results = data.value;
+        console.log(staticData.results);
+    } catch (error) {
+        // output to console log
+        console.log(error.toString());
+    }
+}
