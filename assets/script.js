5c58528d3036ded201d1f7942963787c3d5c84cc
Sheikh <cybardev@pm.me>
2022-02-26 13:11:42 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -6,17 +6,21 @@ const staticData = {
 
 document.querySelector(".search-box")?.addEventListener("submit", (e) => {
     e.preventDefault();
-    fetch(
-        `/.netlify/functions/search?query=${encodeURIComponent(
-            e.currentTarget.searchInput.value
-        )}`
-    )
-        .then((res) => {
-            let data = res.json();
-            console.log(data);
-            staticData.results = data.value;
-        })
-        .catch((err) => {
-            console.log(err.toString());
-        });
+    try {
+        const response = await fetch(
+            `/.netlify/functions/search?query=${encodeURIComponent(
+                e.currentTarget.searchInput.value
+            )}`
+        );
+        if (!response.ok) {
+            // NOT res.status >= 200 && res.status < 300
+            console.log(response.toString());
+        }
+        const data = await response.json();
+        console.log(data);
+        staticData.results = data.value;
+    } catch (error) {
+        // output to console log
+        console.log(error.toString());
+    }
 });
