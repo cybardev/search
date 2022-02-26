47a203f26541c239d86d554aa6ef017eb22a21ab
Sheikh <cybardev@pm.me>
2022-02-26 16:12:12 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,7 @@ const staticData = {
     results: [],
 };
 
-// document.querySelector(".search-box").addEventListener("submit", (event) => {
-//     callAPI(event);
-// });
+// document.querySelector(".search-box").addEventListener("submit", callAPI);
 
 async function callAPI(event) {
     // event.preventDefault();
