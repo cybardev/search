0cf2665fec28c917f131419429f379a7883bffad
Sheikh <cybardev@pm.me>
2022-02-26 13:45:12 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,25 @@
 // @ts-nocheck
 const staticData = {
     results: [],
-    callAPI: async (event) => {
-        event.preventDefault();
-        try {
-            const response = await fetch(
-                `/.netlify/functions/search?query=${encodeURIComponent(
-                    event.currentTarget.searchInput.value
-                )}`
-            );
-            if (!response.ok) {
-                // NOT res.status >= 200 && res.status < 300
-                console.log(response.toString());
-            }
-            const data = await response.json();
-            this.results = data.value;
-            console.log(this.results);
-        } catch (error) {
-            // output to console log
-            console.log(error.toString());
-        }
-    },
 };
+
+async function callAPI(event) {
+    event.preventDefault();
+    try {
+        const response = await fetch(
+            `/.netlify/functions/search?query=${encodeURIComponent(
+                event.currentTarget.searchInput.value
+            )}`
+        );
+        if (!response.ok) {
+            // NOT res.status >= 200 && res.status < 300
+            console.log(response.toString());
+        }
+        const data = await response.json();
+        staticData.results = data.value;
+        console.log(staticData.results);
+    } catch (error) {
+        // output to console log
+        console.log(error.toString());
+    }
+}
